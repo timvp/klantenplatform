d9260288a30d3b1e9b0e86752dbfa3434db5a9e7
Tim Van Parys <tim@effict.be>
2011-09-10 15:24:32 +0200

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klantenplatform::Application.config.secret_token = '50cdc19f863983b0a2d3dc5f8c20e9b27d689271f783828257e217cf26ec194436c87dc955479082cd48418b6a9ac1f99d02166656b6e572bebed1824f881eff'
